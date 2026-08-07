263c04972da3e274a5a055d844e4033a5beb9cc7
Justin <littletrace1@gmail.com>
2026-08-07 15:11:34 +0900

    Fix: 물표 1페이지 번호가 다음 페이지로 밀리는 문제 수정
    번호를 문서 흐름에 넣으면 콘텐츠 높이에 더해져 경계선에 걸린 페이지의
    번호 전체가 다음 페이지로 밀림. 페이지 div에 한정된 position:absolute로
    바꿔 흐름에서 분리(높이에 영향 없음)하고, page-break-before를 추가해
    첫 페이지도 다른 페이지와 동일하게 페이지 분할되도록 함.

--- src/App.jsx
+++ src/App.jsx
@@ -526,7 +526,7 @@ function App() {
               order.itemList.forEach((item, i) => { if (i < 37) rows[i] = item; });
 
               return (
-                <div key={order.orderNo} className="print-page" style={{ height: '297mm', width: '210mm', padding: '15mm', boxSizing: 'border-box' }}>
+                <div key={order.orderNo} className="print-page" style={{ height: '297mm', width: '210mm', padding: '15mm', boxSizing: 'border-box', position: 'relative' }}>
                   <div style={{ textAlign: 'left', fontSize: '15px', marginBottom: '10px' }}>
                     &lt;주문번호&gt;&nbsp;&nbsp;&nbsp;&nbsp;{order.orderNo}
                   </div>
@@ -582,7 +582,10 @@ function App() {
                   <div
                     className="print-page-number"
                     style={{
-                      marginTop: '10px',
+                      position: 'absolute',
+                      bottom: '10mm',
+                      left: 0,
+                      right: 0,
                       textAlign: 'center',
                       fontSize: '11px',
                       color: 'black',
